66b7321aaf2430003aa29f5b9933795d552dbda2
Yishai Zehavi <zehaviyishai@gmail.com>
2020-11-29 16:25:38 +0200

    centered result image

--- src/ui/components/result-image/result-image.component.tsx
+++ src/ui/components/result-image/result-image.component.tsx
@@ -1,44 +1,47 @@
+import React, { useRef, useEffect, useCallback } from 'react';
 
-import React, { useRef, useEffect, useCallback } from 'react'
+import { inject, observer } from 'mobx-react';
 
-import { inject, observer } from 'mobx-react'
-
-import MainStore from '../../../stores/main.store'
-import { Dimensions } from '../../../models/Query-result.model'
+import MainStore from '../../../stores/main.store';
+import { Dimensions } from '../../../models/Query-result.model';
 
 interface ResultImageProps {
-    mainStore?: MainStore
+  mainStore?: MainStore;
 }
 
 const ResultImage = inject('mainStore')(
-    observer((props: ResultImageProps) => {
-        const canvasRef = useRef<HTMLCanvasElement>(null)
+  observer((props: ResultImageProps) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-        const draw = useCallback((ctx: CanvasRenderingContext2D, image: string, rect: Dimensions) => {
-            const img = new Image()
-            img.src = image
+    const draw = useCallback(
+      (ctx: CanvasRenderingContext2D, image: string, rect: Dimensions) => {
+        const img = new Image();
+        img.src = image;
 
-            ctx.drawImage(img, 0, 0)
+        const startPositionX = canvasRef.current!.width / 2 - img.width / 2;
+        ctx.drawImage(img, startPositionX, 0);
 
-            ctx.beginPath()
-            ctx.rect(rect.x, rect.y, rect.width, rect.height)
-            ctx.lineWidth = 2
-            ctx.strokeStyle = 'yellow'
-            ctx.stroke()
-        }, [])
+        ctx.beginPath();
+        ctx.rect(startPositionX + rect.x, rect.y, rect.width, rect.height);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'yellow';
+        ctx.stroke();
+      },
+      [],
+    );
 
-        useEffect(() => {
-            const { image, result } = props.mainStore!
-            if (canvasRef?.current && image && result) {
-                const canvas = canvasRef.current
-                const context = canvas.getContext('2d')!
+    useEffect(() => {
+      const { image, result } = props.mainStore!;
+      if (canvasRef?.current && image && result) {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d')!;
 
-                draw(context, image, result.location)
-            }
-        }, [draw, props.mainStore])
+        draw(context, image, result.location);
+      }
+    }, [draw, props.mainStore]);
 
-        return <canvas ref={canvasRef} />
-    })
-)
+    return <canvas ref={canvasRef} />;
+  }),
+);
 
-export default ResultImage
+export default ResultImage;
